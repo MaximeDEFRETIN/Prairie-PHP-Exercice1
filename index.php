49afd42f13bfd344f93fe715ad66a3b476a7f1f5
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-09-29 15:50:04 +0200

    Exercice 1 corrigé

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 
 <?php
-$chiffre1 = 1;
-$chiffre2 = 3;
-
-echo ($chiffre1 + $chiffre2);
-
-echo ($chiffre1 * $chiffre2);
+  $chiffre1 = 1;
+  $chiffre2 = 3;
+  echo ($chiffre1 + $chiffre2) . '<br />';
+  echo ($chiffre1 * $chiffre2);
 ?>
